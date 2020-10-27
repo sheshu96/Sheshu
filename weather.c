4c30c595d02ea2044a6a0ddebe35a4c4abdb1d2a
SheshuArabati <73385927+SheshuArabati@users.noreply.github.com>
2020-10-27 17:38:16 +0530

    Weather program using command line arguments

--- weather.c
+++ weather.c
@@ -4,34 +4,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int print_temperature();
-int main ()
+int print_temperature(char* );
+int main (int argc, char* args[])
 {
-  	print_temperature();
-  	return 0;
+	print_temperature(args[1]);
+  return 0;
 }
 
-int print_temperature()
+int print_temperature(char* arg)
 {
-	char city[30];
-    char command[500];
-    char data[1000];
+  char city[30];
+  char copy_city[30];
+  char command[500];
+  char data[1000];
+  if(arg == NULL)
+  {
     printf("Enter city name: ");
     scanf("%s", city);
-    sprintf(command, "curl \"http://api.openweathermap.org/data/2.5/find?q=%s&appid=b286785c5f4f54f7ef296483269b6358&units=metric\" >WeatherReport.dat -s", city);
-    system(command);
-    FILE* fp_data = fopen("WeatherReport.dat", "r");
-    fread(data, sizeof(data), 1, fp_data);
-    char separators[5] = "{:[,";
-    char* separated_data = strtok(data, separators);
-    while(separated_data != NULL)
+    strcpy(copy_city, city);
+  }
+  else
+  {
+    strcpy(copy_city, arg);
+  }
+  sprintf(command, "curl \"http://api.openweathermap.org/data/2.5/find?q=%s&appid=b286785c5f4f54f7ef296483269b6358&units=metric\" >WeatherReport.dat -s", copy_city);
+  system(command);
+  FILE* fp_data = fopen("WeatherReport.dat", "r");
+  fread(data, sizeof(data), 1, fp_data);
+  char separators[5] = "{:[,";
+  char* separated_data = strtok(data, separators);
+  while(separated_data != NULL)
+  {
+    if(strcmp(separated_data, "\"temp\"") == 0)
     {
-   		if(strcmp(separated_data, "\"temp\"") == 0)
-   		{
-	   		separated_data = strtok(NULL, separators);
-   			printf("The temparature in %s is %s.", city, separated_data);
-   		}
-   		separated_data = strtok(NULL, separators);
+      separated_data = strtok(NULL, separators);
+      printf("The temparature in %s is %s%cC.", copy_city, separated_data, 248);
     }
-    return(0);
+    separated_data = strtok(NULL, separators);
+  }
+  return(0);
 }
