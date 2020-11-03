a0ef4c651077160978b0d70d663ed501e7f1b0fd
SheshuArabati <73385927+SheshuArabati@users.noreply.github.com>
2020-11-03 11:58:49 +0530

    Weather report program in python

--- weather.py
+++ weather.py
@@ -4,6 +4,6 @@ import requests
 
 location = input("Enter your location to find temperature: ")
 url = "http://api.openweathermap.org/data/2.5/find?appid=d12cf8c5c3c40febafd1dca5750f7eb1&units=metric&q=" + location
-weather_info = requests.get(url).json()
-temp_from_weather_info = weather_info['list'][0]['main']['temp']
-print("Temperature in " + location + " is : " + str (temp_from_weather_info) + chr(176) + "C")
+weather_data = requests.get(url).json()
+temperature = weather_data['list'][0]['main']['temp']
+print("Temperature in " + location + " is: ", temperature, chr(176) + "C")
