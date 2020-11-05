8dc7ff6cd90f7771f41ef6ee0af49f1cee537b19
SheshuArabati <73385927+SheshuArabati@users.noreply.github.com>
2020-11-05 17:39:49 +0530

    Dictionary program using type and speak

--- Dictionary.py
+++ Dictionary.py
@@ -2,7 +2,9 @@
 
 import json
 import os
+from playsound import playsound
 import requests
+import speech_recognition as sr 
 
 def print_definition():
 	definition = response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions']
@@ -13,34 +15,43 @@ def print_synonym():
 
 def get_audio():
 	audio_url = response['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
-	command = "vlc -I null --play-and-exit " + audio_url
-	os.system(command)
+	playsound(audio_url)
 
 def print_sentence():
 	print(response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['examples'][0]['text'])
 
-user_name = input("Enter username: ")
-if user_name.lower() == "sheshu":
-	password = input("Enter password: ")
-	if password == "1234":
-		functions = [print_definition, print_synonym, get_audio, print_sentence, exit]
-		while True:
-			word = input("Enter a word: ")
-			url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/en-gb/" + word
-			response = requests.get(url, headers = {"app_id" : "e7d1b891", "app_key" : "1c03fc176dab15bc72d3908825bba27c"}).json()
-			print("1. Definition\n2. Synonym\n3. Audio\n4. Sentence\n5. Exit")
-			user_option = int(input("Choose your option: "))
-			if user_option == 5:
-				print("Are you sure? ")
-				print("1. Exit\n2. Cancel")
-				exit_option = int(input("Choose your option: "))
-				if exit_option == 1:
-					exit()
-			elif user_option > 0 and user_option < 5:
-				functions[user_option - 1]()
-			else:
-				print("Invalid, please enter a valid option.")
-	else:
-		print("Invalid password.")
+def get_word_by_speak():
+	print("Tell a word: ")
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		audio_data = r.record(source, duration = 2)
+		word = r.recognize_google(audio_data)
+		print(word)
+		return word
+
+def get_word_by_type():
+	word = input("Enter a word: ")
+	return word
+
+functions = [print_definition, print_synonym, get_audio, print_sentence]
+print("Do you want to: ")
+print("1. Type\n2. Speak")
+option = int(input("Choose your option: "))
+if option == 1:
+	word = get_word_by_type()
+elif option == 2:
+	word = get_word_by_speak()
+url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/en-gb/" + word
+response = requests.get(url, headers = {"app_id" : "e7d1b891", "app_key" : "1c03fc176dab15bc72d3908825bba27c"}).json()
+print("1. Definition\n2. Synonym\n3. Audio\n4. Sentence\n5. Exit")
+user_option = int(input("Choose your option: "))
+if user_option == 5:
+	print("Are you sure? ")
+	print("1. Exit\n2. Cancel")
+	exit_option = int(input("Choose your option: "))
+	if exit_option == 1:
+		exit()
+elif user_option > 0 and user_option < 5:
+	functions[user_option - 1]()
 else:
-	print("Invalid user name.")
+	print("Invalid, please enter a valid option.")
