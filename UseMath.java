0f52e4d9e98bd745aa667f4f179de1cb74f9ba8f
SheshuArabati <73385927+SheshuArabati@users.noreply.github.com>
2020-12-04 18:38:31 +0530

    final commit

---
+++ UseMath.java
@@ -0,0 +1,13 @@
+// Main Class
+
+class UseMath
+{
+	public static void main(String args[])
+	{
+		NewMath obj = new NewMath();
+		obj.add(2,4);
+		obj.add(5,3,2);
+		obj.mul(4,5);
+		obj.square(5);
+	}
+}
