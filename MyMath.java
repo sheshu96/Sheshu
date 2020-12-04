57e0d5b8b3b7d2439b8963eea9ebc75294f67a31
SheshuArabati <73385927+SheshuArabati@users.noreply.github.com>
2020-12-04 18:37:27 +0530

    final commit

---
+++ MyMath.java
@@ -0,0 +1,13 @@
+// Program
+
+class MyMath
+{
+	public void add(int number1, int number2)
+	{
+		System.out.println(number1 + number2);
+	}
+	public void mul(int number1, int number2)
+	{
+		System.out.println(number1 * number2);
+	}
+}
